a01b9ef7c6cb28cb040c670627a8e6fc3edf89fe
Parneet Singh <psingh@psingh-mbp.local>
2018-10-17 23:25:55 -0400

    created initial User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180917000420) do
+ActiveRecord::Schema.define(version: 20181018030951) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20180917000420) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

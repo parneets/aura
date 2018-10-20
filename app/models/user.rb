288401d2d78635703e4758078ca902427b19db43
Parneet Singh <psingh@psingh-mbp.local>
2018-10-20 13:01:16 -0400

    added secure password to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: URI::MailTo::EMAIL_REGEXP },
                     uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181018034527) do
+ActiveRecord::Schema.define(version: 20181020161534) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20181018034527) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
